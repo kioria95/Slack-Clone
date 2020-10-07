7ee4b9e7a81eba2c454bf8665f07c0e4f663d867
kioria95 <kenkioria@gmail.com>
2020-10-07 12:14:31 +0300

    Added header.js, sidebar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      
-      
+      {/*Header */}
+      <Header />
+
+      {/*Sidebar */}
+      <Sidebar />
+
+      {/*Header */}
+
+      {/*Header */}
     </div>
   );
 }
